82b830578fafb29ed1b58c333ff55502a84a57ad
Adinalew <164947299+Adinalew@users.noreply.github.com>
2024-10-06 17:07:42 -0400

    added a menu option in main

--- src/vacationPlanner/Main.java
+++ src/vacationPlanner/Main.java
@@ -1,5 +1,19 @@
 package vacationPlanner;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please choose one of the following options:");
+		System.out.println("1. Calculator");
+		System.out.println("2. Exit");
+		int choice = input.nextInt();
+		
+		while (choice < 1 || choice > 2) {
+			System.out.println("Invalid. Please enter a number from 1-2.");
+			choice = input.nextInt();
+		}
+	}
 }

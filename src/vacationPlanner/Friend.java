90ff4642b475b5a59e07bc55001fb3e9f5d7f87e
SStarkman <sstarkma@student.touro.edu>
2024-10-27 12:41:10 -0400

    Added to the main menu - added input validation for non-numeric inputs

--- src/vacationPlanner/Friend.java
+++ src/vacationPlanner/Friend.java
@@ -6,17 +6,17 @@ public class Friend implements Comparable<Friend>{
 	private ArrayList<Item> items;
 	private double totalSpent;
 	
-	public Friend() {
-		friendName = "unknown";
-		items = new ArrayList<Item>();
+	public Friend(String name) {
+		this(name, new ArrayList<Item>());
 	}
 	
-	public Friend(ArrayList<Item> items) { //deep copy
+	public Friend(String name, ArrayList<Item> items) { //deep copy
 		ArrayList<Item> items2 = new ArrayList<>();
 		for(int i = 0; i < items.size(); i++) {
 			items2.add(items.get(i));
 		}
-		items = items2;
+		this.friendName = name;
+		this.items = items2;
 	}
 	
 	public void setFriendName(String friendName) {
@@ -28,7 +28,12 @@ public class Friend implements Comparable<Friend>{
 	}
 	
 	public ArrayList<Item> getItems() {
-		return items;
+		// return a deep copy of the ArrayList
+		ArrayList<Item> copy = new ArrayList<>();
+		for(int i = 0; i < items.size(); i++) {
+			copy.add(this.items.get(i));
+		}
+		return copy;
 	}
 	
 	public double getTotalSpent() {
@@ -41,6 +46,7 @@ public class Friend implements Comparable<Friend>{
 	
 	@Override
 	public int compareTo(Friend other) {
+		//TODO
 		return 0;
 	}
 	
--- src/vacationPlanner/Main.java
+++ src/vacationPlanner/Main.java
@@ -5,15 +5,65 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		int numOfMainMenuChoices = 2;  // Number of options in the main menu - to be updated as menu is updated
+		int choice = 0;                // Placeholder
+		boolean exit = false;
 		
+		do {                // Do while choice is not to exit
+		do {                // Do while choice not a choice in the menu
+		displayMainMenu();  // Display the main menu
+		
+			/*
+			 * Get User Numeric Choice will get a number from the user.
+			 * If the user enters a non-numeric character, the menu will return -1, triggering the while loop to run again
+			 * The while will ensure that the choice is 1 or higher and lower than the number of menu choices
+			 */
+			choice = getUserNumericChoice(input);
+		}while(choice < 1 || choice > numOfMainMenuChoices);
+		
+		switch(choice) {
+		case 1:    // 1. Calculator
+			calculator(input);
+			break;
+		case 2:    // 2. Exit
+			System.out.println("Exiting...");
+			exit = true;
+			break;
+		default:
+			System.out.println("Error");
+		}
+		}while(!exit);
+		
+	}
+
+	/**
+	 * This method will display the main menu
+	 */
+	public static void displayMainMenu() {
 		System.out.println("Please choose one of the following options:");
 		System.out.println("1. Calculator");
 		System.out.println("2. Exit");
-		int choice = input.nextInt();
-		
-		while (choice < 1 || choice > 2) {
-			System.out.println("Invalid. Please enter a number from 1-2.");
-			choice = input.nextInt();
-		}
+	}
+	
+	/**
+	 * @param input Scanner
+	 * This method ensures that the response from the user is fully numeric
+	 * @return Integer response, -1 if the String is not completely numeric
+	 */
+	public static int getUserNumericChoice(Scanner input) {
+		   String answer = input.nextLine();
+		   char[] array = answer.toCharArray();
+		   for(int index = 0; index < array.length; index++) {
+			  if(!Character.isDigit(array[index])) {
+				  System.out.println("Not a digit.");
+				  return -1; // return an integer value that would cause the do-while loop to run again, thereby getting a new user response
+			  } 
+		   }
+		   return Integer.parseInt(answer);
+		   
+	   }
+	
+	public static void calculator(Scanner input) {
+	 System.out.println("To-Do - Calculator");
 	}
 }

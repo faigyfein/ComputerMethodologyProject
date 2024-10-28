a1449cd972343b6da5c6e34237e01eec0062341f
faigy <faigyfein@gmail.com>
2024-10-27 20:36:15 -0400

    Refactored and improved Main class.

--- src/vacationPlanner/Main.java
+++ src/vacationPlanner/Main.java
@@ -10,63 +10,85 @@ public class Main {
 		boolean exit = false;
 		
 		do {                // Do while choice is not to exit
-		do {                // Do while choice not a choice in the menu
-		displayMainMenu();  // Display the main menu
+			displayMainMenu();
+			choice = getIntegerInput(input);			
 		
-			/*
-			 * Get User Numeric Choice will get a number from the user.
-			 * If the user enters a non-numeric character, the menu will return -1, triggering the while loop to run again
-			 * The while will ensure that the choice is 1 or higher and lower than the number of menu choices
-			 */
-			choice = getUserNumericChoice(input);
-		}while(choice < 1 || choice > numOfMainMenuChoices);
-		
-		switch(choice) {
-		case 1:    // 1. Calculator
-			ArrayList<Friend> friends = getAllFriends(input);
-			System.out.println(Calculator.calculate(friends));
-			break;
-		case 2:    // 2. Exit
-			System.out.println("Exiting...");
-			exit = true;
-			break;
-		default:
-			System.out.println("Error");
-		}
+			switch(choice) {
+			case 1:    // 1. Calculator
+				ArrayList<Friend> friends = getAllFriends(input);
+				System.out.println(Calculator.calculate(friends));
+				break;
+			case 2:    // 2. Exit
+				System.out.println("Exiting...");
+				exit = true;
+				break;
+			default:
+				System.out.println("This is not a valid choice. Please reenter a number between 1 and " + numOfMainMenuChoices + ".");
+			}
 		}while(!exit);
 		
 	}
 
 	/**
-	 * This method will display the main menu
+	 * This method will display the main menu.
 	 */
 	public static void displayMainMenu() {
 		System.out.println("Please choose one of the following options:");
 		System.out.println("1. Calculator");
 		System.out.println("2. Exit");
 	}
-	
 	/**
-	 * @param input Scanner
-	 * This method ensures that the response from the user is fully numeric
-	 * @return Integer response, -1 if the String is not completely numeric
+	 * This method validates that the response from the user is an integer, if it is not an integer it continues to reprompt the user until the 
+	 * response is an integer.
+	 * @param input Scanner for reading user input.
+	 * @return The integer value from the user.
 	 */
-	public static int getUserNumericChoice(Scanner input) {
-		   String answer = input.nextLine();
-		   char[] array = answer.toCharArray();
-		   for(int index = 0; index < array.length; index++) {
-			  if(!Character.isDigit(array[index])) {
-				  System.out.println("Not a digit.");
-				  return -1; // return an integer value that would cause the do-while loop to run again, thereby getting a new user response
-			  } 
-		   }
-		   return Integer.parseInt(answer);
-		   
-	   }
-	
+	public static int getIntegerInput(Scanner input) {
+		int answer = 0;
+		boolean validInput = false;
+		while(!validInput) {
+			try {
+				answer = input.nextInt();
+				validInput = true;
+			}catch (InputMismatchException e) {
+				System.out.println("That is not an integer. Please enter an inetger.");
+				input.nextLine(); //clear invalid input
+			}
+		}
+		input.nextLine(); //clear buffer
+		return answer;
+	}
+	/**
+	 * This method validates that the response from the user is a double, if it not a double it continues to reprompt the user until the response
+	 * is a double.
+	 * @param input Scanner for reading user input.
+	 * @return The double value from the user.
+	 */
+	public static double getDoubleInput(Scanner input) {
+		double answer = 0.0;
+		boolean validInput = false;
+		while(!validInput) {
+			try {
+				answer = input.nextDouble();
+				validInput = true;
+			}catch (InputMismatchException e) {
+				System.out.println("Invalid input. Please try again: ");
+				input.nextLine(); //clear invalid input
+			}
+		}
+		input.nextLine(); //clear buffer 
+		return answer;
+		
+	}
+	/**
+	 * This method collects a list of friends and their purchased items.
+	 * @param input Scanner for reading user input.
+	 * @return An ArrayList of Friend objects.
+	 */
 	public static ArrayList<Friend> getAllFriends(Scanner input) {
-		boolean addFriends = true;
 		ArrayList<Friend> friends = new ArrayList<>();
+		
+		boolean addFriends = true;
 		while(addFriends) {
 			System.out.print("Friend Name: >> ");
 			String name = input.nextLine();
@@ -75,48 +97,68 @@ public class Main {
 			addFriends = getYesOrNoChoice(input, "Continue adding friends? ");
 		}
 		return friends;
-		
 	}
-	
+	/**
+	 * This method collects items purchased by a Friend.
+	 * @param input Scanner for reading user input.
+	 * @return An ArrayList of Item objects.
+	 */
 	public static ArrayList<Item> getFriendItems(Scanner input){
-		boolean addItems = true;
 		ArrayList<Item> items = new ArrayList<>();
 		System.out.println("Enter Items Bought: ");
+		
+		boolean addItems = true;
 		while(addItems) {
-			// Get Item Name
 			System.out.print("Item Name >> ");
 			String itemName = input.nextLine();
 			
-			// Get Price
-			// TODO - Input Validation
 			System.out.print("Item Price >> ");
-			double price = input.nextDouble();
-			input.nextLine(); // Clear Buffer
+			double price = getDoubleInput(input);
 			
-			// Get Quantity
-			int quantity;
-			do {
 			System.out.print("Item Quantity >> ");
-			quantity = getUserNumericChoice(input);
-			}while(quantity < 1);
+			int quantity = verifyQuantity(input);
+			
 			
 			items.add(new Item(itemName, price, quantity));
-			
 			addItems = getYesOrNoChoice(input, "Continue adding items?");
 		}
 		return items;
 	}
-	
+	/**
+	 * This method verifies that the quantity is a valid positive integer.
+	 * @param input Scanner for reading user input.
+	 * @return The validated quantity.
+	 */
+	public static int verifyQuantity(Scanner input) {
+		int quantity = 0;
+		boolean validInput = false;
+		
+		while(!validInput) {
+			quantity = getIntegerInput(input);
+			if(quantity >= 1) {
+				validInput = true;
+			}else {
+				System.out.println("Invalid quantity. Must be 1 or greater.");
+			}
+		}
+		return quantity;
+	}
+	/**
+	 * This method prompts the user for a yes('Y') or no('N') choice.
+	 * @param input Scanner for reading user input.
+	 * @param message The passed in message to display.
+	 * @return If this user chooses 'Y' then true, false otherwise.
+	 */
 	public static boolean getYesOrNoChoice(Scanner input, String message) {
 		char answer = 'Y';
 		do {
-		System.out.print(message);
-		System.out.print(" Y / N >> ");
-		answer = input.nextLine().toUpperCase().charAt(0);
+			System.out.print(message);
+			System.out.print(" Y / N >> ");
+			answer = input.nextLine().toUpperCase().charAt(0);
 		}while(answer != 'Y' && answer != 'N');
 		
 		if(answer == 'Y') {
-		return true;
+			return true;
 		}
 		
 		return false;
